c232e62a7a4cd8049e6dc560f3eee9139a0ce238
Amir Bagheri <amirkbagheri@gmail.com>
2022-07-01 15:13:31 +0100

    cleanup

--- examples/beam_cclamped_pyshooting/shooting_opt.py
+++
@@ -1,3 +0,0 @@
-https://docs.scipy.org/doc/scipy/tutorial/optimize.html#broyden-fletcher-goldfarb-shanno-algorithm-method-bfgs
-https://docs.scipy.org/doc/scipy/reference/optimize.minimize-bfgs.html
-https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.minimize.html#scipy.optimize.minimize
